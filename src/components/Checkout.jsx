2652880d37276633508e2027b2072cfe7972375b
Kyle Davelaar <kyle@Kyles-MacBook-Pro.local>
2017-06-18 11:43:47 -0700

    add checkout

---
+++ src/components/Checkout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Navbar from './Navbar';
+
+const Checkout = () => (
+  <div>
+    <Navbar />
+    <h1>Checkout</h1>
+  </div>
+)
+
+export default Checkout;
