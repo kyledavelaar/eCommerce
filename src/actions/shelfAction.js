2ad0c5659da7c221bd0b9a3b3a0b5a05a60c916e
Kyle Davelaar <kyle@Kyles-MacBook-Pro.local>
2017-06-18 11:43:10 -0700

    change images

--- src/actions/shelfAction.js
+++ src/actions/shelfAction.js
@@ -1,40 +1,42 @@
 
 export const populate = () => {
+
+
   return {
     type: 'populate',
     item: [
       {
-        src: "https://unsplash.it/300",
+        src: "https://unsplash.it/300?image=14",
         title: "Blissful Ignorance",
         price: "49",
         id: Math.floor(Math.random() * 1000)
       },
       {
-        src: "https://unsplash.it/300",
+        src: "https://unsplash.it/300?image=18",
         title: "Mountaintop Experience",
         price: "49",
         id: Math.floor(Math.random() * 1000)
       },
       {
-        src: "https://unsplash.it/300",
+        src: "https://unsplash.it/300?image=10",
         title: "Delight and Cherish",
         price: "49",
         id: Math.floor(Math.random() * 1000)
       },
       {
-        src: "https://unsplash.it/300",
+        src: "https://unsplash.it/300?image=20",
         title: "Magical One",
         price: "49",
         id: Math.floor(Math.random() * 1000)
       },
       {
-        src: "https://unsplash.it/300",
+        src: "https://unsplash.it/300?image=21",
         title: "Amazon Rainfall",
         price: "49",
         id: Math.floor(Math.random() * 1000)
       },
       {
-        src: "https://unsplash.it/300",
+        src: "https://unsplash.it/300?image=32",
         title: "Valley Below",
         price: "49",
         id: Math.floor(Math.random() * 1000)
