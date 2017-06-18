52126aeeef811bbab672864eed89e6eb289bbcee
Kyle Davelaar <kyle@Kyles-MacBook-Pro.local>
2017-06-18 08:04:01 -0700

    make navbar ul

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './App.css';
+import '../App.css';
 
 const NavBar = () => (
 
-<nav className="navbar navbar-light bg-faded">
-      <Link className="nav-item nav-link active" to={"/"}>Home<span className="sr-only">(current)</span></Link>
-      <Link className="nav-item nav-link" to={"/cart"}>Cart</Link>
-      <Link className="nav-item nav-link" to={"/about"}>About</Link>
-</nav>
+  <nav className="navbar">
+    <ul>
+      <li><Link className="link" to={"/"}>Home</Link></li>
+      <li><Link className="link" to={"/cart"}>Cart</Link></li>
+      <li><Link className="link" to={"/about"}>About</Link></li>
+    </ul>
+  </nav>
 
-   
-   
+
+
 )
 
 export default NavBar;
