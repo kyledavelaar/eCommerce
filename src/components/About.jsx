feeb8a69d73f4c4a3f8a70b0e986a9a637d84575
Kyle Davelaar <kyle@Kyles-MacBook-Pro.local>
2017-06-18 11:52:34 -0700

    add two columns to about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,9 +2,19 @@ import React from 'react';
 import Navbar from './Navbar';
 
 const About = () => (
-  <div>
-    <Navbar />
-    <h1>About</h1>
+  <div className="container-fluid">
+    <div className="row">
+      <Navbar />
+      <div className="col-sm-12 header">
+        <h1>About</h1>
+      </div>
+      {/*<div className="col-sm-8 col-sm-offset-2">
+        <img src="https://unsplash.it/1200/800?image=0" alt="" className="img-responsive"/>
+      </div>*/}
+      <div className=" text col-sm-8 col-sm-offset-2">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt voluptatem incidunt aliquam veritatis, deserunt quisquam magni quae quasi tenetur, nemo quia optio molestiae amet soluta consequatur blanditiis aut. Error fugit ipsam nesciunt earum amet natus saepe minus omnis dignissimos autem placeat, numquam iure totam eaque dolores necessitatibus laborum maxime labore voluptates vel incidunt fuga assumenda doloribus aspernatur?  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur fugiat, odit adipisci quidem quaerat numquam ipsum repellat id dolorem similique perspiciatis omnis, molestias rem! Totam praesentium nobis et sit, repellat porro quos numquam assumenda ullam harum. Quod, praesentium id, earum cum inventore voluptates magni nulla. Error, vero, animi dolorum adipisci velit eveniet quisquam maiores fugiat.</p>
+      </div>
+    </div>
   </div>
 )
 
