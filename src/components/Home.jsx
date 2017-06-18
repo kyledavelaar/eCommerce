5c2e5c1f6aadbd4ac1f8f596cac57bd33a8f267d
Kyle Davelaar <kyle@Kyles-MacBook-Pro.local>
2017-06-18 08:37:56 -0700

    add list items

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,9 +2,25 @@ import React from 'react';
 import Navbar from './Navbar';
 
 const Home = () => (
-  <div>
-    <Navbar />
-    <h1>Home</h1>
+  <div className="container-fluid">
+    <div className="row">
+      <Navbar />
+      <div className="col-sm-12 header">
+        <h1>Home</h1>
+      </div>
+        <ul className="list">
+          <li className="col-sm-4 list-item">
+            <img className="img-responsive" src="https://unsplash.it/200" alt="purchase"/>
+            <h4>Caption</h4>
+          </li>
+          <li className="col-sm-4 list-item">
+            <img className="img-responsive" src="https://unsplash.it/200" alt="purchase"/>
+          </li>
+          <li className="col-sm-4 list-item">
+            <img className="img-responsive" src="https://unsplash.it/200" alt="purchase"/>
+          </li>
+        </ul>
+    </div>
   </div>
 )
 
