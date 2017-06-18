ca3565fdcff2ed06cbf3a92b7f7b10865835cd3c
Kyle Davelaar <kyle@Kyles-MacBook-Pro.local>
2017-06-18 13:00:10 -0700

    media query and box-shadow for nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const NavBar = () => (
 
   <nav className="navbar">
-    <ul>
+    <ul className="nav-ul">
       <li><Link className="link" to={"/"}>Home</Link></li>
       <li><Link className="link" to={"/cart"}>Cart</Link></li>
       <li><Link className="link" to={"/about"}>About</Link></li>
